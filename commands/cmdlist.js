3c298c0df49d957e183b0393f05c95ad0e2a13d5
kiraseira <nlt-gh-omegalul@protonmail.com>
2021-01-26 17:51:05 +0100

    new commands: command list and help

--- commands/cmdlist.js
+++ commands/cmdlist.js
@@ -1,19 +1,28 @@
 exports.kirabot_command = {
-		name: "",
-		desc: "",
-		help: "",
-		aliases: null,
+		name: "commandlist",
+		desc: "List of available commands",
+		help: "Prints a list of available commands",
+		aliases: ["commands", "cmds"],
 		userlevel: 0,
 		pingsender: 1,
 		cds: {
-				user: 5,
+				user: 10,
 				channel: 10
 			},
 		code: function(sender, lparam) {
 				return new Promise((resolve, reject) => {
-					resolve("NYI");
-				
-				
+					let result = "commands(aliases): ";
+					ksb.cmds.forEach(cmd => {
+						result += cmd.name;
+						if(cmd.aliases){
+							result+="(";
+							result+=cmd.aliases.join(", ");
+							result+=")";
+						}
+						result += ", ";
+					});
+					resolve(result.substr(0, result.length-2));
+					return;
 				});
 				}
 }
