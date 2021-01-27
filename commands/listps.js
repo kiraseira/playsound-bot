69c48859bcab492a07082f8e47025d6105175dd3
kiraseira <nlt-gh-omegalul@protonmail.com>
2021-01-27 12:17:31 +0100

    add ordering and small fix in list playsounds command

--- commands/listps.js
+++ commands/listps.js
@@ -11,13 +11,13 @@ exports.kirabot_command = {
 			},
 		code: function(sender, lparam) {
 				return new Promise((resolve, reject) => {
-					let sdata = ksb.db.syncSelect(`SELECT * FROM playsounds WHERE enabled='1';`);
+					let sdata = ksb.db.syncSelect(`SELECT * FROM playsounds WHERE enabled='1' ORDER BY name;`);
 					if (!sdata || sdata.length===0){
 						resolve(`there are no enabled playsounds Saj`);
 						return;
 					}
 					let pss = "";
-					for(let i=0; i<sdata.length-1;i++){
+					for(let i=0; i<sdata.length;i++){
 						pss += sdata[i].name+" ";
 					}
 					resolve(`Available playsounds (total: ${sdata.length}): ${pss}`);
